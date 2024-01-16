323c8de4781e4fbe2687d8cc1cbb831bb5c27928
jin <jjin_0627@hotmail.com>
2024-01-15 16:23:51 -0800

    added section links

---
+++ components/resume.tsx
@@ -0,0 +1,11 @@
+const resume = () => {
+
+    return (
+        <div>
+            <h2>blah blah</h2>
+            <p>yadda yadda</p>
+        </div>
+    );
+}
+
+export default resume;
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,14 +1,12 @@
-import hero from "../../components/hero";
 import intro from "../../components/intro";
-import content from '../../components/content';
+import content from "../../components/content";
 
 const HomePage = () => {
-  const heroComponent = hero();
   const contentComponent = content();
   const introComponent = intro();
-  
+
   return (
-    <div className='container mx-auto px-40'>
+    <div className="container mx-auto px-40">
       {introComponent}
       {contentComponent}
     </div>
