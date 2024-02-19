5d925bce81250281e1d95d5237c87531dc9b50b6
jin <jjin_0627@hotmail.com>
2024-02-18 22:13:54 -0800

    updated portfolio link + tried to add favicon

--- components/nav.tsx
+++ components/nav.tsx
@@ -9,7 +9,7 @@ const nav = () => {
         <Link className="link-style" href="/about">
           about
         </Link>
-        <Link className="link-style" href="https://www.portfolio.jinlee.info">
+        <Link className="link-style" href="https://portfolio.jinlee.info/">
           portfolio
         </Link>
         <Link className="link-style" href="https://open.spotify.com/user/jinherondole?si=a78f4d4454b245fa">
